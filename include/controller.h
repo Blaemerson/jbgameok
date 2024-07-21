921717dc829736636863796465cf982f1cad1027
MyHubForYou <joeysavage2000@gmail.com>
2024-07-20 21:43:45 -0500

    added level generate and player

---
+++ include/controller.h
@@ -0,0 +1,23 @@
+#include <SFML/Window/Keyboard.hpp>
+
+using namespace sf; // for graphics
+
+Keyboard keyboard;
+
+
+class Controller 
+{
+
+    switch (event.type == sf::Event::KeyPressed) 
+    {
+
+        case event.key.scancode == sf::scancode::Escape:: 
+        {
+
+        }
+        
+
+
+    }
+
+}
